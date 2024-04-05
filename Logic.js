ceb5f4028dd2e388bcc30ee65d40b6545313fee7
Bramastio Bagus Aji Wijaya <152799278+Bramastio447@users.noreply.github.com>
2024-04-05 16:04:52 +0700

    V.1.x
    menambah code agar lebih lengkap

--- Logic.js
+++ Logic.js
@@ -3,3 +3,28 @@ let buku = [
     { "nama": "lupin", "jumlah": 2 },
     { "nama": "aladin", "jumlah": 2 }
 ];
+
+function periksaBuku() {
+    const jumlahInput = document.getElementById('jumlah');
+    const jumlah = parseInt(jumlahInput.value);
+    const pilihanBuku = document.getElementById('buku').value;
+
+    if (jumlah === '' || isNaN(jumlah)) {
+        alert('Jumlah buku harus diisi!');
+        return;
+    }
+
+    const bukuDipilih = buku.find(buku => buku.nama === pilihanBuku);
+
+    if (!bukuDipilih) {
+        alert('Buku tidak ditemukan!');
+        return;
+    }
+
+    if (jumlah > bukuDipilih.jumlah) {
+        alert('Stok buku tidak mencukupi!');
+        return;
+    }
+
+    alert('Silahkan ambil buku anda!');
+}
